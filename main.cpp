df3f963b7bcb791ff3cde4152b7cd98993c7102d
darkazazeal <dk242@kent.ac.uk>
2016-04-05 10:40:12 +0100

    Added bearing and speed pids. Cleaned up and moved the motor to own function.

--- main.cpp
+++ main.cpp
@@ -8,8 +8,14 @@
 
 #include "boat.pb.h"
 
-#define ESC_ADDRESS 0x2B
+#define ESC_ADDRESS_0 0x2B
+#define ESC_ADDRESS_1 0x2A
 #define RATE 0.1
+
+// PID Limits
+#define Rpm_Limit 8913.0
+#define Throttle_Limit 32767.0
+#define Speed_In_Knots_Limit 3.5
 
 // Test for correctness when in a room that does produce it's own magnetic field...
 // Magnetic Declination for Canterbury = WEST 0deg 16min
@@ -20,8 +26,15 @@ Serial usbSerial(USBTX, USBRX);
 I2C i2c(D14, D15);
 DigitalOut heartbeat(LED_GREEN);
 HMC5883L compass(i2c);
-SimonK_I2C_ESC motor_1(i2c, ESC_ADDRESS,6);
+
+SimonK_I2C_ESC motor_1(i2c, ESC_ADDRESS_1,6);
+SimonK_I2C_ESC motor_2(i2c, ESC_ADDRESS_2,6);
+
+// PID declarations
+PID speed_over_ground_pid(0.5, 1, 0, RATE);
+PID heading_pid(0.5, 1, 0, RATE);
 PID motor_1_pid(0.5, 1, 0, RATE);
+PID motor_2_pid(0.5, 1, 0, RATE);
 
 // We are using the nanopb version of protocol buffers.
 shedBoat_Telemetry telemetry_message = shedBoat_Telemetry_init_zero;
@@ -36,16 +49,35 @@ int main() {
     heartbeat_tkr.attach_us(&beat, 250000);
 
     i2c.frequency (400);
-    motor_1_pid.setInputLimits(0.0,  8913.0);
-    motor_1_pid.setOutputLimits(0.0, 32767.0);
+
+		// Initialise PIDs
+    speed_over_ground_pid.setInputLimits(0.0,  Speed_In_Knots_Limit); // Assuming speed is in knots -- check this!
+    speed_over_ground_pid.setOutputLimits(0.0, 1.0);
+    speed_over_ground_pid.setMode(AUTO_MODE);
+
+    heading_pid.setInputLimits(0,360);
+    heading_pid.setOutputLimits(0.0, 1.0);
+    heading_pid.setMode(AUTO_MODE);
+
+    motor_1_pid.setInputLimits(0.0,  Rpm_Limit);
+    motor_1_pid.setOutputLimits(0.0, Throttle_Limit);
     motor_1_pid.setMode(AUTO_MODE);
 
-    // Arm the motor
+    motor_2_pid.setInputLimits(0.0,  Rpm_Limit);
+    motor_2_pid.setOutputLimits(0.0, Throttle_Limit);
+    motor_2_pid.setMode(AUTO_MODE);
+
+		// Initialise Motors (Arming Sequence) -- If a value is not sent periodicially, then the motors WILL disarm!
     motor_1.set(0);
+		motor_2.set(0);
     wait(1);
 
-    motor_1_pid.setSetPoint(3000);
+		speed_over_ground_pid.setSetPoint(2.5);
+    heading_pid.setSetPoint(startBearing(degToRad(NMEA::getLatitude()), degToRad(NMEA::getLongitude()), degToRad(51.298997), degToRad(1.056683))*(180.0/M_PI));
 
+		// These will need removing once we have the aggregating function.
+		motor_1_pid.setSetPoint(3000);
+		motor_2_pid.setSetPoint(3000);
 
 	usbSerial.printf(	"     _              _   _                 _   \r\n"
 						"    | |            | | | |               | |  \r\n"
@@ -69,20 +101,10 @@ int main() {
 	while(1)
     {
 		wait_ms(500);
-
-        motor_1.update();
-
-        if(motor_1.isAlive()){
-            motor_1_pid.setProcessValue(motor_1.rpm());
-            usbSerial.printf("%d",motor_1.rpm());
-            usbSerial.printf(" Actual RPM\t\t");
-            float rpm_compensation = motor_1_pid.compute();
-            usbSerial.printf("%f",rpm_compensation);
-            usbSerial.printf("Rpm Compensation\t\t\r\n");
-            motor_1.set((short)rpm_compensation);
-        }
-        send_telemetry();
-
+		updateHeading();
+		updateSpeedOverGround();
+	  updateMotors();
+    send_telemetry();
 
 		usbSerial.printf("Time:       %02d:%02d:%02d\r\n", NMEA::getHour(), NMEA::getMinute(), NMEA::getSecond());
 		usbSerial.printf("Satellites: %d\r\n", NMEA::getSatellites());
@@ -92,7 +114,7 @@ int main() {
 		usbSerial.printf("Speed:      %0.2fkm/h\r\n", NMEA::getSpeed());
 		usbSerial.printf("Bearing:    %0.2f degrees\r\n", NMEA::getBearing());
 		heading = compass.smoothedHeading();
-        usbSerial.printf("Compass Bearing: %03.0f\r\n", heading);
+    usbSerial.printf("Compass Bearing: %03.0f\r\n", heading);
 		usbSerial.printf("Heading Delta to South: %03.0f\r\n", heading_delta(heading, 180.0));
 		usbSerial.printf("Distance to Parkwood %06.2fm\r\n",
 			equirectangular(degToRad(NMEA::getLatitude()), degToRad(NMEA::getLongitude()),
@@ -121,7 +143,40 @@ void send_telemetry()
     telemetry_message.location.latitude = NMEA::getLatitude();
     telemetry_message.location.longitude = NMEA::getLongitude();
     telemetry_message.location.number_of_satellites_visible = NMEA::getSatellites();
+		telemetry_message.location.true_heading = heading;
     telemetry_message.status = shedBoat_Telemetry_Status_UNDEFINED;
     telemetry_message.motor[0].motor_number = 1;
     telemetry_message.motor[0].rpm = motor_1.rpm();
+}
+
+void updateSpeedOverGround(){
+    speed_over_ground_pid.setProcessValue(NMEA::getSpeed());
+    float speed_over_ground_compensation = speed_over_ground_pid.compute(); // Need to pass this to a function aggregating bearing correction and speed.
+}
+
+void updateHeading(){
+    heading_pid.setProcessValue(compass.smoothedHeading());     // Please rename the function call to bearing.
+    float direction_compensation = heading_pid.compute(); // Need to pass this to a function aggregating bearing correction and speed.
+}
+
+void updateMotors(){
+        // Grab new data from the motors
+        motor_1.update();
+        motor_2.update();
+        // If a motor is responsive, then calculate new throttle compensation and pass this to the motor. Also report back.
+        if(motor_1.isAlive()){
+            motor_1_pid.setProcessValue(motor_1.rpm());
+            pc.printf("%d",motor_1.rpm());pc.printf(" Actual Motor1 RPM\t\t");
+            float throttle_compensation = motor_1_pid.compute();
+            pc.printf("%f",throttle_compensation);pc.printf(" Throttle Compensation Motor1\t\t");
+            motor_1.set((short)throttle_compensation);
+        }
+        if(motor_2.isAlive()){
+            motor_2_pid.setProcessValue(motor_2.rpm());
+            pc.printf("%d",motor_2.rpm());pc.printf(" Actual Motor2 RPM\t\t");
+            float throttle_compensation_2 = motor_2_pid.compute();
+            pc.printf("%f",throttle_compensation_2);pc.printf(" Throttle Compensation Motor2\t\t");
+            pc.printf("\n\r");
+            motor_2.set((short)throttle_compensation_2);
+        }
 }
